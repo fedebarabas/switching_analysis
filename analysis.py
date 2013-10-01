b7bd07daa5db208fb4a4ad7ebfc7f421235c030f
Federico Barabas <barabas@df.uba.ar>
2013-10-01 20:22:17 +0200

    OOP framework done, needs plot fixing

--- analysis.py
+++ analysis.py
@@ -12,48 +12,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+def expo(x, a, b):
+            return a * np.exp(-x * b)
 
 class Data:
     """Methods for analyzing the switching dynamics data"""
 
-#    def __init__(self):
-
-    def load(self, file_name):
+    def load(self, file_name, bins=50):
         """Data loading"""
 
         self.file_name = file_name
         path_list = file_name.split(os.sep)
-        parameter = file_name[file_name.rindex('_') + 1:len(file_name)]
-        dt = np.dtype([(parameter, '<f4'), ('molecules', '<f4')])
-        table = np.fromfile(file_name, dtype=dt)
+        self.path = path_list[-2] + r"/" + path_list[-1]
+        self.parameter = file_name[file_name.rindex('_') + 1:len(file_name)]
+        dt = np.dtype([(self.parameter, '<f4'), ('molecules', '<f4')])
+        self.table = np.fromfile(file_name, dtype=dt)
 
-        return table
+        # HISTOGRAM CONSTRUCTION
+        self.hist, bin_edges = np.histogram(self.table[self.parameter],
+                                                    bins=bins)
+        self.bin_centres = (bin_edges[:-1] + bin_edges[1:]) / 2
+        self.bin_width = bin_edges[1] - bin_edges[0]
 
-    def plot(self, table):
-
-        # HISTOGRAM
-        hist, bin_edges = np.histogram(table[parameter], bins=50)
-        bin_centres = (bin_edges[:-1] + bin_edges[1:]) / 2
-        width = bin_edges[1] - bin_edges[0]
+    def plot(self):
+        """Data plotting"""
 
         # PLOT THE HISTOGRAM
         fig = plt.figure()
         #ax = fig.add_subplot(111)
-        plt.title(path_list[-2] + r"/" + path_list[-1])
-        plt.bar(bin_centres, hist, width, alpha=0.5, label=parameter)
-        plt.xlabel(parameter)
+        plt.title(self.path)
+        plt.bar(self.bin_centres, self.hist, self.bin_width, alpha=0.5,
+                                                        label=self.parameter)
+        plt.xlabel(self.parameter)
         plt.grid(True)
         plt.legend()
+        plt.show()
+        self.plot = fig        # not really working
 
-        #plt.show()
-        return fig
+    def fit(self, fit_start=0):
+        """Histogram fitting"""
 
-    def fit(self, table, ):
-        ## HISTOGRAM FITTING
-        #def expo(x, a, b):
-            #return a * np.exp(-x * b)
-
-        #guess = [hist[0], 1 / table[parameter].mean()]
+        guess = [self.hist[0], 1 / np.mean(self.table[self.parameter])]
         ## Should I use the sqrt as a sigma for the photons?
         #if parameter in ['photons', 'totalphotons']:
             #sigma = np.sqrt(hist[5:30])
@@ -64,34 +63,37 @@ class Data:
         #else:
             #coeff, var_matrix = curve_fit(expo, bin_centres[5:30], hist[5:30],
                 #p0=guess)
-        ##coeff, var_matrix = curve_fit(expo, bin_centres[5:30], hist[5:30], p0=guess)
-        #hist_fit = expo(bin_centres, *coeff)
+        coeff, var_matrix = curve_fit(expo, self.bin_centres[fit_start:-1],
+                                      self.hist[fit_start:-1], p0=guess)
+        hist_fit = expo(self.bin_centres, *coeff)
 
-        #print(guess)
-        #print('Fitted amplitude = ', coeff[0])
-        #print('Fitted lambda = ', coeff[1])
-        #print('Fitted 1/lambda = ', 1 / coeff[1])
+        print(guess)
+        print('Fitted amplitude = ', coeff[0])
+        print('Fitted lambda = ', coeff[1])
+        print('Fitted 1/lambda = ', 1 / coeff[1])
 
-        ## PLOT
-        #width = bin_edges[1] - bin_edges[0]
-        #plt.title(path_list[-2] + r"/" + path_list[-1])
-        #plt.bar(bin_centres, hist, width, alpha=0.5, label='_nolegend_')
-        #plt.plot(bin_centres[5:30], hist_fit[5:30], color='r', lw=3,
-            #label="y = A * exp(- B * x)\nA = {}\n1/B = {}"
-            #.format(int(coeff[0]), int(1 / coeff[1])))
-        #plt.xlabel(parameter)
-        #plt.grid(True)
-        #plt.legend()
-
-        #plt.show()
+        # PLOT
+        plt.title(self.path)
+        plt.bar(self.bin_centres, self.hist, self.bin_width, alpha=0.5,
+                                                        label='_nolegend_')
+        plt.plot(self.bin_centres[fit_start:-1], hist_fit[fit_start:-1],
+                color='r', lw=3,
+                label="y = A * exp(- B * x)\nA = {}\n1/B = {}"
+                .format(int(coeff[0]), int(1 / coeff[1])))
+        plt.xlabel(self.parameter)
+        plt.grid(True)
+        plt.legend()
+        plt.show()
 
 if __name__ == "__main__":
     datos = Data()
     stringi = (r"\\hell-fs\STORM\Switching\130612_sdab_a647_poc_mea"
     r"\r60mw_uv0p1_113hz_000.sw_photons")
-    fig = datos.load(stringi)
-    fig.show()
-    sys.exit(0)
+    datos.load(stringi)
+    print(datos.path)
+    datos.plot()
+    print("heyhey")
+    #datos.fit()
 
 
 
