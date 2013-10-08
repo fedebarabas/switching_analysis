3903774238811ffbdfe798bef80cd30a3888574c
Federico Barabas <barabas@df.uba.ar>
2013-10-08 20:27:59 +0200

    Joining, plotting and results storing improved

--- analysis.py
+++ analysis.py
@@ -11,8 +11,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-    # TODO: DETECT AND MERGE MEASUREMENTS OF THE SAME DAY AND POWER
-
 def expo(x, A, inv_tau):
     return A * np.exp(- inv_tau * x)
 
@@ -32,37 +30,63 @@ def load_dir(initialdir='\\\\hell-fs\\STORM\\Switching\\', parameter='photons'):
     except OSError:
         print("No folder selected!")
 
+    # Load list of files of the required type
     file_list = glob("*.sw_" + parameter)
 
-    dots = [file_list[i].find('.') for i in range(len(file_list))]
+    # Look for duplicates and put them in the same list
+    return_list = []
 
-    file_list[i][0:dots[i]] # PART TO FIND IN THE OTHER ITEMS
+    for file in file_list:
+        matching = [s for s in file_list if file[0:file.find('.')-3] in s]
+        return_list.append(matching)
 
-    # TODO: return_list SHOULD BE A LIST OF LISTS OF THINGS
-    #       THAT HAVE TO BE JOINED
+    return_list =  [list(t) for t in set(map(tuple, return_list))]
 
     return dir_name, return_list
 
 class Data:
     """Methods for analyzing the switching dynamics data"""
 
-    def load(self, file_name, bins=50):
-        """Data loading"""
+    def load(self, dir_name, file_name, bins=50):
+        """Data loading
+        file_name can be:
+            ~) a string containing the name of the file to load
+            ~) a list of strings, containing the names of the files that
+            should be considered part of the same dataset"""
 
-        # TODO: JOIN LISTS OF FILES
-
-        # Paths and parameter extraction
+        self.subdir = os.path.split(dir_name)[1]
         self.file_name = file_name
-        tmp_path = os.path.split(file_name)
-        tmp_path2 = os.path.split(tmp_path[0])
-        self.path = tmp_path2[1] + r"/" + tmp_path[1]
-        self.parameter = file_name[file_name.rindex('_') + 1:len(file_name)]
-        self.power = tmp_path[1][1:tmp_path[1].index('_') - 2]
-        self.date = tmp_path2[1][0:6]
+        self.date = self.subdir[0:6]
 
-        # Data loading
-        dt = np.dtype([(self.parameter, '<f4'), ('molecules', '<f4')])
-        self.table = np.fromfile(file_name, dtype=dt)
+        if type(file_name) is list:
+            # Paths and parameter extraction
+            self.path = [os.path.join(dir_name, file) for file in file_name]
+            self.joined = True
+            self.nfiles = len(file_name)
+            self.minipath = (self.subdir + r"/" +
+                                    file_name[0][0:file_name[0].find('.') - 4])
+            self.parameter = file_name[0][file_name[0].rindex('_')
+                                                        + 1:len(file_name[0])]
+            self.power = file_name[0][1:file_name[0].index('_') - 2]
+
+            # Data loading
+            dt = np.dtype([(self.parameter, '<f4'), ('molecules', '<f4')])
+            self.table = np.empty((1), dtype=dt)
+            for file in file_name:
+                self.table = np.concatenate((self.table,
+                                             np.fromfile(file, dtype=dt)))
+            self.table = self.table[1:-1]
+
+        else:
+            # Paths and parameter extraction
+            self.joined = False
+            self.minipath = self.subdir + r"/" + file_name
+            self.parameter = file_name[file_name.rindex('_') + 1:len(file_name)]
+            self.power = file_name[1:file_name.index('_') - 2]
+
+            # Data loading
+            dt = np.dtype([(self.parameter, '<f4'), ('molecules', '<f4')])
+            self.table = np.fromfile(file_name, dtype=dt)
 
         # Histogram construction
         self.mean = np.mean(self.table[self.parameter])
@@ -103,11 +127,15 @@ class Data:
         """Data plotting"""
 
         self.fig, self.ax = plt.subplots()
-        self.ax.set_title(self.path)
+        self.ax.set_title(self.minipath)
         self.ax.bar(self.bin_centres, self.hist, self.bin_width, alpha=0.5,
                     label='_nolegend_')
         self.ax.set_xlabel(self.parameter)
         self.ax.grid(True)
+        self.ax.text(0.75 * self.ax.get_xlim()[1], 0.2 * self.ax.get_ylim()[1],
+        "Number of counts:\n" + str(self.table.size),
+                    horizontalalignment='center', verticalalignment='center',
+                    bbox=dict(facecolor='white'))
 
         # If the histogram was fit, then we plot also the fitting exponential
         if self.fitted:
@@ -115,8 +143,8 @@ class Data:
             self.ax.plot(self.bin_centres[self.fit_start:-1],
                      hist_fit[self.fit_start:-1],
                      color='r', lw=3,
-                     label="y = A * exp(-inv_tau * x)\nA = {}\n1/inv_tau = {}"
-                     .format(int(self.amplitude), int(self.inv_tau)))
+                     label="y = A * exp(-inv_tau * x)\nA = {}\ntau = {}"
+                     .format(int(self.amplitude), int(self.tau)))
             self.ax.legend()
 
 
@@ -127,17 +155,14 @@ if __name__ == "__main__":
     # Creation of an instance of Data() for each file
     data_list = [Data() for file in file_list]
 
-    powers = []
-    results = []
-    dates = []
+    results = np.empty([len(file_list), 3])
 
-    # Process all files in a loop
+#   Process all files in a loop
     for i in range(len(file_list)):
-        data_list[i].load(os.path.join(dir_name, file_list[i]))
+        data_list[i].load(dir_name, file_list[i])
         data_list[i].fit(2)
-        powers.append(data_list[i].power)
-        results.append(data_list[i].tau)
-        dates.append(data_list[i].date)
+        data_list[i].plot()
+        results[i] = [data_list[i].date, data_list[i].tau, data_list[i].power]
 
     print(data_list[0].parameter + " analyzed in " + dir_name)
     print(results)
